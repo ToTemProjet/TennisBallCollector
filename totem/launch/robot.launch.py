eae85a7ba7d88f73cf2777582a57901fa4c390af
gauriefl <florian.gaurier@ensta-bretagne.org>
2022-01-25 10:02:08 +0100

    Préparation spawn robot

--- totem/launch/robot.launch.py
+++ totem/launch/robot.launch.py
@@ -18,6 +18,11 @@ def generate_launch_description():
     tennis_court_share = get_package_share_directory("tennis_court")
     gazebo_ros_share = get_package_share_directory("gazebo_ros")
     cam_zen_share = get_package_share_directory("camera_zenitale")
+    # desc_package = ""
+    # desc_pkg = FindPackageShare(desc_package).find(desc_package)
+    # model_file = os.path.join(desc_pkg, "urdf", "robot.urdf.xacro")
+
+    # os.system("ros2 service call /delete_entity 'gazebo_msgs/DeleteEntity'  '{name: totem}'")
 
     # Tennis court
     tennis_court_launch_file = os.path.join(tennis_court_share, "launch", "tennis_court.launch.py")
@@ -35,8 +40,33 @@ def generate_launch_description():
         executable="zenithal_camera"
     )
 
+    # robot_state_publisher_node = Node(
+    #     package="robot_state_publisher", executable="robot_state_publisher",
+    #     parameters=[{"robot_description": Command(["xacro", " ", model_file])}]
+    #     )
+
+    # gazebo_spawn_entity_node = Node(
+    #     package="gazebo_ros", executable="spawn_entity.py",
+    #     arguments=["-entity", "super_robot_4000", "-topic", "/robot_description",
+    #                 "-x", "0", "-y", "0", "-z", "0.95"],
+    #     )
+
+    # rqt_robot_steering_node = Node(
+    #     package="rqt_robot_steering", executable="rqt_robot_steering"
+    #     )
+
+    # joint_state_publisher_node = Node(
+    #     package="joint_state_publisher",
+    #     executable="joint_state_publisher"
+    #     )
+
+
     return LaunchDescription([
         DeclareLaunchArgument(name="ROS_DISTRO", default_value="foxy"),
         tennis_court_launch,
         zen_cam
+        # robot_state_publisher_node,
+        # gazebo_spawn_entity_node,
+        # rqt_robot_steering_node,
+        # joint_state_publisher_node
     ])
