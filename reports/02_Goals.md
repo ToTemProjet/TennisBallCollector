232482e18d9f3fa7358618ed31bf49194044a789
gauriefl <florian.gaurier@ensta-bretagne.org>
2022-01-25 08:47:48 +0100

    day 2

---
+++ reports/02_Goals.md
@@ -0,0 +1,22 @@
+# Objectifs du 21/01/2022
+
+PO: Florian Gaurier
+
+
+## Fonctionnalités attendues
+
+Architecture physique
+Aller jusqu'à la balle
+
+
+## Tâches à réaliser
+
+Détection des balles
+Construire le robot
+Mapping
+
+
+## Challenges techniques
+
+Gestion de la perspective
+
--- totem/camera_zenitale/camera_zenitale/zenithal_camera.py
+++ totem/camera_zenitale/camera_zenitale/zenithal_camera.py
@@ -4,10 +4,12 @@ from sensor_msgs.msg import Image
 import cv2
 import numpy as np
 
+
 class zenithalCameraNode(Node):
 	def __init__(self):
 		super().__init__("zenithal_camera")
 		self.camera_history = 1
+		self.display = False
 
 		camera_qos_policy = rclpy.qos.QoSProfile(reliability=rclpy.qos.ReliabilityPolicy.BEST_EFFORT,
 														  history=rclpy.qos.HistoryPolicy.KEEP_LAST,
@@ -18,15 +20,22 @@ class zenithalCameraNode(Node):
 	def img_callback(self, msg):
 		self.img = np.array(msg.data, dtype=np.uint8).reshape((msg.height, msg.width, 3))
 		self.img = cv2.cvtColor(self.img, cv2.COLOR_RGB2BGR)
-		cv2.imshow("test", self.img)
-		cv2.waitKey(1)
+		if self.display == True:
+			cv2.imshow("Camera zenitale", self.img)
+			cv2.waitKey(1)
 
 	def serachBalls(self):
 		"""Returns array of [X, Y, T] for each ball detected,T is the time since ball has spawned"""
+		
+
+
 		pass
 
+	def shutdown(self, sig, frame):
+		sys.exit(0)
 
 def main():
 	rclpy.init()
 	zen_cam_node = zenithalCameraNode()
+	signal.signal(signal.SIGINT, zen_cam_node.shutdown)
 	rclpy.spin(zen_cam_node)
