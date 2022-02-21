5a975394214a5a595f271ded265c8e7f04f892c8
gauriefl <florian.gaurier@ensta-bretagne.org>
2022-02-21 11:12:02 +0100

    Balls detection OK

--- totem/camera_zenitale/camera_zenitale/zenithal_camera.py
+++ totem/camera_zenitale/camera_zenitale/zenithal_camera.py
@@ -1,76 +1,11 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
+from std_msgs.msg import Float32MultiArray, MultiArrayDimension
 import cv2
 import numpy as np
-<<<<<<< HEAD
 import signal
 import sys
-from matplotlib import pyplot as plt
-
-
-def plot3d(img, name, fig=None):
-	# y, x = np.where(img>=0)
-	# values = img[y, x]
-	x, y = np.meshgrid(np.linspace(0, img.shape[1], img.shape[1]), np.linspace(0, img.shape[0], img.shape[0]))
-	values = img
-	print(y)
-	print(y.shape)
-	print(x.shape)
-	print(img.shape)
-
-	if fig == None:
-		fig = plt.figure()
-		ax = fig.gca(projection='3d')
-	else:
-		ax = fig.gca(projection='3d')
-		ax.clear()
-	ax.set_xlim3d(src.shape[1], 0)
-	ax.set_ylim3d(0, src.shape[0])
-	ax.plot_surface(x, y, values, cmap='viridis', edgecolor='none')
-
-	ax.set_title(name)
-
-	return fig
-
-def plot3dchans(img, name, fig=None):
-	img = np.where((np.sum(img, axis=2).reshape((img.shape[0], img.shape[1], 1))>0), img, np.nan)
-	if fig == None:
-		fig = plt.figure()
-		ax = fig.gca(projection='3d')
-	else:
-		ax = fig.gca(projection='3d')
-		ax.clear()
-	ax.set_xlim3d(0, 255)
-	ax.set_ylim3d(0, 255)
-	ax.set_zlim3d(0, 255)
-	ax.scatter(img[:,:,0], img[:,:,1], img[:,:,2], cmap='viridis', edgecolor='none')
-	ax.set_title(name)
-	return fig
-
-def plot3dlabeled(img, name, labels, fig=None):
-	colors = cm.viridis(np.linspace(0, 1, labels.max()+1))
-
-
-	img = np.where((np.sum(img, axis=2).reshape((img.shape[0], img.shape[1], 1))>0), img, np.nan)
-	if fig == None:
-		fig = plt.figure()
-		ax = fig.gca(projection='3d')
-	else:
-		ax = fig.gca(projection='3d')
-		ax.clear()
-
-	ax.set_xlim3d(0, 255)
-	ax.set_ylim3d(0, 255)
-	ax.set_zlim3d(0, 255)
-	for i in range(0,labels.max()+1):
-		subimage = img[np.where(labels==i)]
-		ax.scatter(subimage[:,0], subimage[:,1], subimage[:,2], label= str(i))
-
-	ax.set_title(name)
-	ax.legend()
-
-	return fig
 
 
 class zenithalCameraNode(Node):
@@ -85,10 +20,11 @@ class zenithalCameraNode(Node):
 														  depth=self.camera_history)
 		
 		self.img_subscriber = self.create_subscription(Image, "/zenith_camera/image_raw", self.img_callback, camera_qos_policy)
+		self.ball_publisher = self.create_publisher(Float32MultiArray, "balls_position", 10)
 
 		self.create_timer(0.1, self.search_balls_and_robot)
 
-		self.thresh = [80, 150]
+		self.thresh = [30, 50]
 
 	def img_callback(self, msg):
 		self.img = np.array(msg.data, dtype=np.uint8).reshape((msg.height, msg.width, 3))
@@ -99,32 +35,49 @@ class zenithalCameraNode(Node):
 
 	def search_balls_and_robot(self):
 		"""Returns array of [X, Y, T] for each ball detected,T is the time since ball has spawned"""
-		try:
+		if self.img is not None:
 			# yuv = cv2.cvtColor(self.img, cv2.COLOR_BGR2YUV)
 			# cielab = cv2.cvtColor(self.img, cv2.COLOR_BGR2Lab)
 			hsv = cv2.cvtColor(self.img, cv2.COLOR_BGR2HSV)
 
 			# self.fig = plot3dchans(hsv, "hsv", self.fig)
-			thresholded = cv2.threshold(hsv[:,:,0], self.thresh[0], self.thresh[1], cv2.THRESH_BINARY)
+			thresholded = cv2.inRange(hsv[:,:,0], (self.thresh[0]), (self.thresh[1]))
 
-			# cv2.imshow("thresholded", thresholded)
-			cv2.imshow("hsv", hsv)
-			cv2.imshow("h", hsv[:, :, 0])
-			cv2.imshow("s", hsv[:, :, 1]) #
-			cv2.imshow("v2", hsv[:, :, 2])
-			cv2.waitKey(1)
-		except:
-			pass
+			output = cv2.connectedComponentsWithStats(thresholded)
+			(numLabels, labels, stats, centroids) = output
 
 
+			if self.display == True:
+				thresholded = cv2.cvtColor(thresholded, cv2.COLOR_GRAY2BGR)
+				for centroid in centroids[1:]:
+					cv2.circle(thresholded, (int(centroid[0]), int(centroid[1])), 2, (0, 0, 255), -1)
+
+				cv2.imshow("t", thresholded)
+				cv2.waitKey(1)
+
+			arg = Float32MultiArray()
+			thing = MultiArrayDimension()
+			thing.label = "x"
+			thing.size = 1
+			thing.stride = 1
+			arg.layout.dim.append(thing)
+
+			thing = MultiArrayDimension()
+			thing.label = "y"
+			thing.size = 1
+			thing.stride = 1
+			arg.layout.dim.append(thing)
+
+			# arg.layout.data_offset = 1
+			arg.data = []
+			for centroid in centroids[1:]:
+				arg.data.append(centroid[0])
+				arg.data.append(centroid[1])
+
+			self.ball_publisher.publish(arg)
 
 	def shutdown(self, sig, frame):
 		sys.exit(0)
-	def img_callback(self, msg):
-		self.img = np.array(msg.data, dtype=np.uint8).reshape((msg.height, msg.width, 3))
-		self.img = cv2.cvtColor(self.img, cv2.COLOR_RGB2BGR)
-		cv2.imshow("test", self.img)
-		cv2.waitKey(1)
 
 def main():
 	rclpy.init()
