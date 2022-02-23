77179f8d99c7ccf4291708c8bc728c413fd08dcf
gauriefl <florian.gaurier@ensta-bretagne.org>
2022-02-23 10:59:50 +0100

    Source setup.sh fonctionnel

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-source /opt/ros/$(ROS_DISTRO)/setup.bash
+source /opt/ros/galactic/setup.bash || source /opt/ros/foxy/setup.sh
 export LC_NUMERIC="en_US.UTF-8"
 . install/setup.bash
 # export ROS_DOMAIN_ID=24
